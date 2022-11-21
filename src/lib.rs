d683edd7de339a13223ecf38a85d405fc2a80913
david-thylacine-network <david@thylacine.network>
2022-11-21 15:34:30 +0800

    add some fun

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
 use reqwest::Error;
 use serde_derive::Deserialize; 
 use serde_json::json;
+use tokio::runtime::Runtime;
 
+pub fn tx_raw_sync(network: String, tx_hex: String) -> Result<String, Error>{
+    let rt = Runtime::new().unwrap();
+    let future_fn = tx_raw(network, tx_hex);
+    rt.block_on(future_fn)
+}
 
 pub async fn tx_raw(network: String, tx_hex: String) -> Result<String, Error>{
     let request_url = format!(
@@ -26,6 +32,12 @@ pub struct ScriptHistory{
     height: u32,
 }
 
+pub fn script_history_sync(network: String, script_hash: String) -> Result<Vec<ScriptHistory>, Error>{
+    let rt = Runtime::new().unwrap();
+    let future_fn = script_history(network, script_hash);
+    rt.block_on(future_fn)
+}
+
 pub async fn script_history(network: String, script_hash: String) -> Result<Vec<ScriptHistory>, Error>{
     let request_url = format!(
         "https://api.whatsonchain.com/v1/bsv/{network}/script/{script_hash}/history",
@@ -35,6 +47,68 @@ pub async fn script_history(network: String, script_hash: String) -> Result<Vec<
     let response = reqwest::get(&request_url).await?;
     let ret: Vec<ScriptHistory> = response.json().await?;
     Ok(ret)
+}
+
+pub fn script_unspent_sync(network: String, script_hash: String) -> Result<Vec<ScriptHistory>, Error>{
+    let rt = Runtime::new().unwrap();
+    let future_fn = script_unspent(network, script_hash);
+    rt.block_on(future_fn)
+}
+
+pub async fn script_unspent(network: String, script_hash: String) -> Result<Vec<ScriptHistory>, Error>{
+    let request_url = format!(
+        "https://api.whatsonchain.com/v1/bsv/{network}/script/{script_hash}/unspent",
+        network=network,
+        script_hash=script_hash
+    );
+    let response = reqwest::get(&request_url).await?;
+    let ret: Vec<ScriptHistory> = response.json().await?;
+    Ok(ret)
+}
+
+#[derive(Deserialize, Debug, PartialEq, Eq)]
+pub struct Unspent{
+    height: u32,
+    tx_pos: u16,
+    tx_hash: String,
+    value: u32,
+}
+
+#[derive(Deserialize, Debug, PartialEq, Eq)]
+pub struct ScriptsUnspent{
+    script: String,
+    unspent: Vec<Unspent>,
+    error: String,
+}
+
+pub fn scripts_unspent_sync(network: String, scripts: &Vec<String>) -> Result<Vec<ScriptsUnspent>, Error>{
+    let rt = Runtime::new().unwrap();
+    let future_fn = scripts_unspent(network, scripts);
+    rt.block_on(future_fn)
+}
+
+pub async fn scripts_unspent(network: String, scripts: &Vec<String>) -> Result<Vec<ScriptsUnspent>, Error>{
+    let request_url = format!(
+        "https://api.whatsonchain.com/v1/bsv/{network}/scripts/unspent",
+        network=network
+    );
+    let params = json!({ "scripts": scripts }).to_string();
+    println!("params: {:?}", params);
+    
+    let client = reqwest::Client::new();
+    let response = client.post(&request_url)
+        .header("Content-Type", "application/json")
+        .body(params)
+        .send()
+        .await?;
+    let ret: Vec<ScriptsUnspent> = response.json().await?;
+    Ok(ret)
+}
+
+pub fn woc_sync(network: String) -> Result<bool, Error> {
+    let rt = Runtime::new().unwrap();
+    let future_fn = woc(network);
+    rt.block_on(future_fn)
 }
 
 pub async fn woc(network: String) -> Result<bool, Error> {
